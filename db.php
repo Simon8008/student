cec54babe3b201a2f143b53f5cda1bef321466c1
Simon8008 <115245804+Simon8008@users.noreply.github.com>
2024-04-27 03:16:13 +0100

    student web app

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+                //connect to database
+                $servername = "group-30-database-1.cluster-cswwgr31dvqw.us-east-1.rds.amazonaws.com";
+                $username = "admin_Group_30";
+                $password = "Group_30";
+                $database = "Group_30_Student_DB";
+
+                //Create Connection
+                $connection = new mysqli($servername, $username, $password, $database);
+?>
